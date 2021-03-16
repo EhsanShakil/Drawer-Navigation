135f95cbc72835b487dcd2228197733ea15b98d9
EhsanShakil <ahsanshakeel1994@gmail.com>
2021-03-16 23:23:50 +0500

    Updating

--- navigations/stack.js
+++ navigations/stack.js
@@ -1,24 +1,62 @@
+import React from "react";
+import { View, Text, TouchableOpacity, SafeAreaView } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import { createDrawerNavigator } from "react-navigation-drawer";
+import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
 import Home from "../screens/home/Home";
 import Welcome from "../screens/welcome/Welcome";
 import Header from "../components/Header";
-import { TouchableOpacity } from "react-native";
+import Shops from "../screens/shops/Shops";
+import Brands from "../screens/brands/Brands";
+import { Entypo, AntDesign, FontAwesome5 } from "@expo/vector-icons";
+
+const CustomDrawerContentComponent = (props) => (
+  <View>
+    <SafeAreaView>
+      <View style={{ justifyContent: "center", height: 100 }}>
+        <Text style={{ textAlign: "center", fontSize: 20, fontWeight: "bold" }}>
+          Drawer Navigation
+        </Text>
+      </View>
+      <View>
+        <DrawerItems {...props} />
+      </View>
+    </SafeAreaView>
+  </View>
+);
 
 const DrawerNavigator = createDrawerNavigator(
   {
     Welcome: {
       screen: Welcome,
+      navigationOptions: {
+        drawerIcon: <Entypo name="home" size={24} color="black" />,
+      },
     },
     Home: {
       screen: Home,
+      navigationOptions: {
+        drawerIcon: <AntDesign name="home" size={24} color="black" />,
+      },
+    },
+    Shops: {
+      screen: Shops,
+      navigationOptions: {
+        drawerIcon: <Entypo name="shopping-bag" size={24} color="black" />,
+      },
+    },
+    Brands: {
+      screen: Brands,
+      navigationOptions: {
+        drawerIcon: <FontAwesome5 name="store" size={20} color="black" />,
+      },
     },
   },
   {
     // drawerWidth: "100%",
     drawerBackgroundColor: "white",
     overlayColor: "black",
+    contentComponent: CustomDrawerContentComponent,
   }
 );
 const StackNavigator = createStackNavigator(
---
+++ screens/brands/Brands.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Brands = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>BRANDS</Text>
+    </View>
+  );
+};
+
+export default Brands;
